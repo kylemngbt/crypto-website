e12bee9cd91abf842470ec14f3ee8432b73ce046
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-25 15:50:51 +0800

    build: define settings object

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+let settings = {
+  "async": true,
+  "scrossDomain": true,
+  "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+  "method": "GET",
+  "headers": {}
+}
